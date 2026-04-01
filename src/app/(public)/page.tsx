ba6cbd0c9f093f451c5415b701c3535801b87d84
Tarek <Tarek@gmail.com>
2026-04-01 23:14:59 +0200

    feat: PWA + Banner + layout

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabase'
-
+import Banner from '@/components/shared/Banner'
 type UnitType = 'student' | 'family' | 'studio' | 'shared'
 
 type Property = {
